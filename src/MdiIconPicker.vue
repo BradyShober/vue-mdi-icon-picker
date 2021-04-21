35a979857adb3497b158afd80c4c3c6901c3234d
Brady Shober <32466298+BradyShober@users.noreply.github.com>
2021-04-21 11:07:25 -0500

    Update MdiIconPicker.vue
    Added background color to fix transparency issues
    Removed v-tooltip and just used title

--- src/MdiIconPicker.vue
+++ src/MdiIconPicker.vue
@@ -8,18 +8,13 @@
         </v-row>
         <v-row dense style="max-height: 200px; max-width: 300px;">
             <v-col cols="4" v-for="icon in filteredIcons" :key="icon.name">
-                <v-tooltip top>
-                    <template v-slot:activator="{on}">   
-                        <v-icon v-on="on" @click="selectedIcon(icon.name)" large>mdi-{{icon.name}}</v-icon>
-                    </template>
-                    <span>{{icon.name}}</span>
-                </v-tooltip>
+                <v-icon v-on="on" @click="selectedIcon(icon.name)" large :title="icon.name">mdi-{{icon.name}}</v-icon>
             </v-col>
         </v-row>
     </v-menu>
 </template>
 <script lang="ts">
-    import {Component, Vue, Prop} from 'vue-property-decorator';
+    import { Component, Vue, Prop } from 'vue-property-decorator';
 
     @Component({
         components: {}
@@ -32,32 +27,33 @@
         icons: Array<any>
 
         search = ""
-		
-		id = ""
-		
-		created(){
-			this.id = Math.random().toString(36).replace('0.','icon-picker' || '')
-		}
 
-        selectedIcon(icon: string){
+        id = ""
+
+        created() {
+            this.id = Math.random().toString(36).replace('0.', 'icon-picker' || '')
+        }
+
+        selectedIcon(icon: string) {
             this.$emit('select', `mdi-${icon}`);
         }
 
-        updateSearch(e: string){
+        updateSearch(e: string) {
             this.search = e;
         }
 
-        get filteredIcons(){
+        get filteredIcons() {
             return this.icons.filter(i => i.name.includes(this.search) || i.aliases.includes(this.search) || i.tags.includes(this.search));
         }
-		
-		get idQuery(){
-			return `#${this.id}`;
-		}
+
+        get idQuery() {
+            return `#${this.id}`;
+        }
     }
 </script>
 <style scoped>
-	.v-menu__content {
-		min-width: 300px !important;
-	}
+    .v-menu__content {
+        min-width: 300px !important;
+        background-color: white;
+    }
 </style>
